3f5aa761c5516d80e9049b33b562a46ac806134c
bisope03 <bishakhagope789@gmail.com>
2022-09-11 13:07:48 +0530

    Add the getData function to fetch the data from the json

--- script.js
+++ script.js
@@ -1,107 +1,4 @@
-let data = [
-    {
-      "title": "Work",
-      "timeframes": {
-        "daily": {
-          "current": 5,
-          "previous": 7
-        },
-        "weekly": {
-          "current": 32,
-          "previous": 36
-        },
-        "monthly": {
-          "current": 103,
-          "previous": 128
-        }
-      }
-    },
-    {
-      "title": "Play",
-      "timeframes": {
-        "daily": {
-          "current": 1,
-          "previous": 2
-        },
-        "weekly": {
-          "current": 10,
-          "previous": 8
-        },
-        "monthly": {
-          "current": 23,
-          "previous": 29
-        }
-      }
-    },
-    {
-      "title": "Study",
-      "timeframes": {
-        "daily": {
-          "current": 0,
-          "previous": 1
-        },
-        "weekly": {
-          "current": 4,
-          "previous": 7
-        },
-        "monthly": {
-          "current": 13,
-          "previous": 19
-        }
-      }
-    },
-    {
-      "title": "Exercise",
-      "timeframes": {
-        "daily": {
-          "current": 1,
-          "previous": 1
-        },
-        "weekly": {
-          "current": 4,
-          "previous": 5
-        },
-        "monthly": {
-          "current": 11,
-          "previous": 18
-        }
-      }
-    },
-    {
-      "title": "Social",
-      "timeframes": {
-        "daily": {
-          "current": 1,
-          "previous": 3
-        },
-        "weekly": {
-          "current": 5,
-          "previous": 10
-        },
-        "monthly": {
-          "current": 21,
-          "previous": 23
-        }
-      }
-    },
-    {
-      "title": "Self Care",
-      "timeframes": {
-        "daily": {
-          "current": 0,
-          "previous": 1
-        },
-        "weekly": {
-          "current": 2,
-          "previous": 2
-        },
-        "monthly": {
-          "current": 7,
-          "previous": 11
-        }
-      }
-    }
-];
+let data = [];
 
 const buttons = [...document.querySelectorAll('.options')]
 
@@ -109,6 +6,14 @@ const activateClickedButton = (button) => {
     buttons.forEach(b => b.classList.remove('active'));  // To remove the other active buttons
     button.classList.add('active');
 };
+
+const getData = async () => {
+  // fetch data
+  const response = await fetch('./data.json');
+  const fetchedData = await response.json();
+  data = fetchedData
+  buttons[1].click()
+}
 
 const clearActivities = () => {
   // clear the existing cards from the html
@@ -168,3 +73,5 @@ buttons.forEach(button => {
         renderCards(clickedOption);
     })
 });
+
+getData();
